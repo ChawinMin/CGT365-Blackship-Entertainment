872aa13c2715cdab2617bd582d747730273e2398
TheBookofTruth <nicefreshpairofshoes@outlook.com>
2025-12-05 22:59:35 -0500

    Cannon to Ship integration started
    Made an attempt to put the cannons to the ship. Proves to be a lot difficult than I expected...ARGH

---
+++ CullofCthulu/Intermediate/Build/Win64/UnrealEditor/Inc/CullofCthulu/UHT/BP_Cannon.generated.h
@@ -0,0 +1,54 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "BP_Cannon.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef CULLOFCTHULU_BP_Cannon_generated_h
+#error "BP_Cannon.generated.h already included, missing '#pragma once' in BP_Cannon.h"
+#endif
+#define CULLOFCTHULU_BP_Cannon_generated_h
+
+#define FID_BLACKSHIP_ENTERTAINMENT_COC_CullofCthulu_Source_CullofCthulu_BP_Cannon_h_13_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUBP_Cannon(); \
+	friend struct Z_Construct_UClass_UBP_Cannon_Statics; \
+public: \
+	DECLARE_CLASS(UBP_Cannon, UActorComponent, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/CullofCthulu"), NO_API) \
+	DECLARE_SERIALIZER(UBP_Cannon)
+
+
+#define FID_BLACKSHIP_ENTERTAINMENT_COC_CullofCthulu_Source_CullofCthulu_BP_Cannon_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	UBP_Cannon(UBP_Cannon&&); \
+	UBP_Cannon(const UBP_Cannon&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UBP_Cannon); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UBP_Cannon); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UBP_Cannon) \
+	NO_API virtual ~UBP_Cannon();
+
+
+#define FID_BLACKSHIP_ENTERTAINMENT_COC_CullofCthulu_Source_CullofCthulu_BP_Cannon_h_10_PROLOG
+#define FID_BLACKSHIP_ENTERTAINMENT_COC_CullofCthulu_Source_CullofCthulu_BP_Cannon_h_13_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_BLACKSHIP_ENTERTAINMENT_COC_CullofCthulu_Source_CullofCthulu_BP_Cannon_h_13_INCLASS_NO_PURE_DECLS \
+	FID_BLACKSHIP_ENTERTAINMENT_COC_CullofCthulu_Source_CullofCthulu_BP_Cannon_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> CULLOFCTHULU_API UClass* StaticClass<class UBP_Cannon>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_BLACKSHIP_ENTERTAINMENT_COC_CullofCthulu_Source_CullofCthulu_BP_Cannon_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ CullofCthulu/Intermediate/Build/Win64/UnrealEditor/Inc/CullofCthulu/UHT/CullofCthulu.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeCullofCthulu_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_CullofCthulu;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_CullofCthulu()
+	{
+		if (!Z_Registration_Info_UPackage__Script_CullofCthulu.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/CullofCthulu",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0xDFEC2F56,
+				0x201F3694,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_CullofCthulu.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_CullofCthulu.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_CullofCthulu(Z_Construct_UPackage__Script_CullofCthulu, TEXT("/Script/CullofCthulu"), Z_Registration_Info_UPackage__Script_CullofCthulu, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xDFEC2F56, 0x201F3694));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
