f103e321261c2503b21d9bb07cb4ef54eeb6838b
Qu0wa <loldogegamer@gmail.com>
2026-02-24 22:46:03 -0500

    TEST PUSH
    I had to rebuild the project on my end, so all I did was make a universal damage ship event in the Ship Blueprint. Please let me know if the project can't be opened as i have saved a backup

--- CullofCthulu/Intermediate/Build/Win64/x64/CullofCthuluEditor/Development/Engine/SharedPCH.Engine.Project.ValApi.Cpp20.InclOrderUnreal5_3.h
+++ CullofCthulu/Intermediate/Build/Win64/x64/CullofCthuluEditor/Development/Engine/SharedPCH.Engine.Project.ValApi.Cpp20.InclOrderUnreal5_3.h
@@ -1,3 +1,3 @@
 // PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "C:/Users/ajash/Desktop/Purdue/365/CGT365-Blackship-Entertainment/CullofCthulu/Intermediate/Build/Win64/x64/CullofCthuluEditor/Development/Engine/SharedDefinitions.Engine.Project.ValApi.Cpp20.InclOrderUnreal5_3.h"
+#include "D:/CullofCthulhu/Repository/CGT365-Blackship-Entertainment/CullofCthulu/Intermediate/Build/Win64/x64/CullofCthuluEditor/Development/Engine/SharedDefinitions.Engine.Project.ValApi.Cpp20.InclOrderUnreal5_3.h"
 #include "Runtime/Engine/Public/EngineSharedPCH.h"
--- CullofCthulu/Intermediate/Build/Win64/x64/CullofCthuluEditor/Development/UnrealEd/SharedPCH.UnrealEd.Cpp20.h
+++ CullofCthulu/Intermediate/Build/Win64/x64/CullofCthuluEditor/Development/UnrealEd/SharedPCH.UnrealEd.Cpp20.h
@@ -1,3 +1,3 @@
 // PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
-#include "C:/Users/ajash/Desktop/Purdue/365/CGT365-Blackship-Entertainment/CullofCthulu/Intermediate/Build/Win64/x64/CullofCthuluEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
+#include "D:/CullofCthulhu/Repository/CGT365-Blackship-Entertainment/CullofCthulu/Intermediate/Build/Win64/x64/CullofCthuluEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
 #include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
